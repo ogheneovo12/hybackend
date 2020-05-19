90b391514d98e3b5f4ce3c0e6a437e1db35cd59b
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-05-19 22:52:01 +0100

    solved port issue

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require("express")
-const http = require("http")
 const app = express();
 const bodyparser = require("body-parser")
+const graphqlHTTP = require("express-graphql");
+const schema = require("./graphql/Schema")
+const cors = require("cors");
+app.use(cors())
 const PORT = process.env.PORT || 5000
-// const graphqlHTTP = require("express-graphql");
-//const schema = require("./graphql/Schema")
-//const cors = require("cors");
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
+app.use("/graphql",graphqlHTTP({
+    schema,
+    graphiql:true
+   }))
+app.use(express.static("public"))
 
-// app.use(cors())
-
-// app.use(bodyparser.urlencoded({extended:true}))
-// app.use(bodyparser.json())
-
-app.get("/",(req,res)=>{
-    res.send("HEY boy")
-})
-// app.use("/graphql",graphqlHTTP({
-//     schema,
-//     graphiql:true
-// }))
-const server = http.createServer(app);
-server.listen(PORT)
+app.listen(PORT,()=>console.log("servers started at port 5000"))
