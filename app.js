3ffb61d477d572507e1e188a0310ef62dbeff5f1
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-05-19 22:49:44 +0100

    final

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express")
 const http = require("http")
 const app = express();
 const bodyparser = require("body-parser")
+const PORT = process.env.PORT || 5000
 // const graphqlHTTP = require("express-graphql");
 //const schema = require("./graphql/Schema")
 //const cors = require("cors");
 
 // app.use(cors())
 
-app.use(bodyparser.urlencoded({extended:true}))
-app.use(bodyparser.json())
+// app.use(bodyparser.urlencoded({extended:true}))
+// app.use(bodyparser.json())
 
 app.get("/",(req,res)=>{
     res.send("HEY boy")
@@ -19,4 +20,4 @@ app.get("/",(req,res)=>{
 //     graphiql:true
 // }))
 const server = http.createServer(app);
-server.listen(5000)
+server.listen(PORT)
