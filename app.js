e5085bf95acc863c79954a7b20f20c71a028f35f
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-05-19 22:29:19 +0100

    last x

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const http = require("http")
 const app = express();
 const bodyparser = require("body-parser")
-const graphqlHTTP = require("express-graphql");
+// const graphqlHTTP = require("express-graphql");
 const schema = require("./graphql/Schema")
 const cors = require("cors");
 
@@ -10,20 +10,13 @@ const cors = require("cors");
 
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(bodyparser.json())
-app.use("/graphql",graphqlHTTP({
-    schema,
-    graphiql:true
-   }))
+
 app.get("/",(req,res)=>{
     res.end("HEY boy")
 })
-
-const server = http.createServer(app)
-// app.use(express.static("public"))
-
-
-
-
-
-
+// app.use("/graphql",graphqlHTTP({
+//     schema,
+//     graphiql:true
+// }))
+const server = http.createServer(app);
 server.listen(5000,()=>console.log("servers started at port 5000"))
