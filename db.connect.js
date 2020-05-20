37e344bf1d17d4807f701aa7e68697d4ca8a50a8
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-05-20 15:56:05 +0100

    hostia

--- db.connect.js
+++ db.connect.js
@@ -2,7 +2,7 @@
  const db = {
               host: "mysql.freehostia.com",
               user: "ukuogh_hychurch",
-              password: "Benj@_2018",
+              password: "mypass",
               database:"ukuogh_hychurch",
               port:3306,
               multipleStatements: true
